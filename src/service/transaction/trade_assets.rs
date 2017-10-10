5685d48d4c8e01310dd737e0626d92fcfcfbd3ec
Ilya Sinyakin <sinyakin.ilya@gmail.com>
2017-10-10 13:24:06 +0300

    DMB-20: trade transaction was modified;

--- src/service/transaction/trade_assets.rs
+++ src/service/transaction/trade_assets.rs
@@ -3,7 +3,7 @@ extern crate exonum;
 use exonum::blockchain::Transaction;
 use exonum::storage::Fork;
 use exonum::crypto;
-use exonum::crypto::{PublicKey, Signature};
+use exonum::crypto::{PublicKey, Signature, verify};
 use exonum::messages::Message;
 use serde_json::Value;
 
@@ -15,13 +15,12 @@ use service::schema::wallet::WalletSchema;
 pub const FEE_FOR_TRADE: u64 = 1;
 
 encoding_struct! {
-    struct Offer {
-        const SIZE = 112;
+    struct TradeOffer {
+        const SIZE = 48;
 
         field seller:              &PublicKey   [00 => 32]
         field assets:              Vec<Asset>   [32 => 40]
         field price:               u64          [40 => 48]
-        field signature:           &Signature   [48 => 112]
     }
 }
 
@@ -29,34 +28,23 @@ message! {
     struct TxTrade {
         const TYPE = SERVICE_ID;
         const ID = TX_TRADE_ASSETS_ID;
-        const SIZE = 48;
+        const SIZE = 112;
 
-        field buyer:         &PublicKey  [00 => 32]
-        field offer:         Offer       [32 => 40]
-        field seed:          u64         [40 => 48]
-    }
-}
+        field buyer:              &PublicKey    [00 => 32]
+        field offer:              TradeOffer    [32 => 40]
+        field seed:               u64           [40 => 48]
+        field seller_signature:   &Signature    [48 => 112]
 
-encoding_struct! {
-    struct OfferData {
-        const SIZE = 48;
-
-        field seller:              &PublicKey   [00 => 32]
-        field assets:              Vec<Asset>   [32 => 40]
-        field price:               u64          [40 => 48]
     }
 }
 
 impl TxTrade {
     fn offer_verify(&self) -> bool {
-        let offer_data = OfferData::new(
-            self.offer().seller(),
-            self.offer().assets(),
-            self.offer().price()
-        );
-        crypto::verify(
-            self.offer().signature(),
-            &offer_data.raw,
+        *self.buyer() != *self.offer().seller() &&
+        self.verify_signature(self.buyer()) &&
+        verify(
+            self.seller_signature(),
+            &self.offer().raw,
             self.offer().seller()
         )
     }
@@ -112,9 +100,9 @@ fn test_trade_convert_from_json() {
     "offer": {
         "seller": "83dbc25eea26578cfdae481b421b09faeb1b35b98451a30c9a6a33271503e61a",
         "assets": [{"hash_id": "a8d5c97d-9978-4b0b-9947-7a95dcb31d0f", "amount":2}, {"hash_id": "a8d5c97d-9978-4b0c-9947-7a95dcb31d0f", "amount":3}],
-        "price": "10",
-        "signature": "100c4bf9d50bd2da4af8d65b7b35847b0258d59d62b993311af4ce86049fa5de6712847db7b1a62d217e8c289bdf7b151552fac2404f965383c2c07fc39a5409"
+        "price": "10"
     },
+    "seller_signature": "100c4bf9d50bd2da4af8d65b7b35847b0258d59d62b993311af4ce86049fa5de6712847db7b1a62d217e8c289bdf7b151552fac2404f965383c2c07fc39a5409",
     "seed": "123123123123"
   },
   "network_id": 0,
