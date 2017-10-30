bb7c0168bf162da3c8797e07052ca203dee17c39
Ilya Sinyakin <sinyakin.ilya@gmail.com>
2017-10-30 13:18:57 +0200

    fixed: tx_fee for mining

--- src/service/transaction/mining.rs
+++ src/service/transaction/mining.rs
@@ -50,7 +50,7 @@ impl Transaction for TxMining {
     fn info(&self) -> Value {
         json!({
             "transaction_data": self,
-            "tx_fee": FEE_FOR_TRANSFER,
+            "tx_fee": 0,
         })
     }
 
