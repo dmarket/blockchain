76aa78bf2ec36583abc466e5bb9de35ea1c12bb7
Mikola Samardak <niksaak@gmail.com>
2017-11-28 05:00:13 +0200

    DMB-17: rearrange the tests the right way.

--- src/service/transaction/del_assets.rs
+++ src/service/transaction/del_assets.rs
@@ -72,116 +72,121 @@ impl Transaction for TxDelAsset {
 
 
 #[cfg(test)]
-use service::wallet::Wallet;
-#[cfg(test)]
-use exonum::storage::{MemoryDB, Database};
+mod test {
+    use exonum::storage::{MemoryDB, Database};
+    use exonum::blockchain::Transaction;
+    use service::schema::asset::AssetSchema;
+    use service::schema::wallet::WalletSchema;
+    use service::wallet::{Wallet, Asset, AssetInfo};
+    use service::transaction::del_assets::{TxDelAsset, FEE_FOR_MINING};
 
-#[cfg(test)]
-fn get_json() -> String {
-    r#"{
-  "body": {
-    "pub_key": "1d9c731ebac3d7da9482470ae8b13a839cb05ef4f21f8d119e2c4bf175333cf7",
-    "assets": [
-      {
-        "hash_id": "asset_1",
-        "amount": 45
-      },
-      {
-        "hash_id": "asset_2",
-        "amount": 17
-      }
-    ],
-    "seed": "113"
-  },
-  "network_id": 0,
-  "protocol_version": 0,
-  "service_id": 2,
-  "message_id": 4,
-  "signature": "e7a3d71fc093f9ddaba083ba3e1618514c96003d9a01cdf6d5c0da344f12c800db9e7b210f9a7b372ddd7e57f299d8bc0e55d238ad1fa6b9d06897c2bda29901"
-}"#.to_string()
-}
+    fn get_json() -> String {
+        r#"{
+        "body": {
+            "pub_key": "1d9c731ebac3d7da9482470ae8b13a839cb05ef4f21f8d119e2c4bf175333cf7",
+            "assets": [
+                {
+                    "hash_id": "asset_1",
+                    "amount": 45
+                },
+                {
+                  "hash_id": "asset_2",
+                  "amount": 17
+                }
+            ],
+        "seed": "113"
+        },
+        "network_id": 0,
+        "protocol_version": 0,
+        "service_id": 2,
+        "message_id": 4,
+        "signature": "e7a3d71fc093f9ddaba083ba3e1618514c96003d9a01cdf6d5c0da344f12c800db9e7b210f9a7b372ddd7e57f299d8bc0e55d238ad1fa6b9d06897c2bda29901"
+    }"#.to_string()
+    }
 
-#[test]
-fn test_convert_from_json() {
-    let tx_del: TxDelAsset = ::serde_json::from_str(&get_json()).unwrap();
-    assert!(tx_del.verify());
-    assert_eq!(45, tx_del.assets()[0].amount());
-    assert_eq!("asset_2", tx_del.assets()[1].hash_id());
-}
+    #[test]
+    fn test_convert_from_json() {
+        let tx_del: TxDelAsset = ::serde_json::from_str(&get_json()).unwrap();
+        assert!(tx_del.verify());
+        assert_eq!(45, tx_del.assets()[0].amount());
+        assert_eq!("asset_2", tx_del.assets()[1].hash_id());
+    }
 
-#[test]
-fn positive_delete_assets_test() {
-    let tx_del: TxDelAsset = ::serde_json::from_str(&get_json()).unwrap();
+    #[test]
+    fn positive_delete_assets_test() {
+        let tx_del: TxDelAsset = ::serde_json::from_str(&get_json()).unwrap();
 
-    let db = Box::new(MemoryDB::new());
+        let db = Box::new(MemoryDB::new());
 
-    let mut asset_schema = AssetSchema { view: &mut db.fork() };
-    asset_schema.assets().put(
-        &"asset_1".to_string(),
-        AssetInfo::new(tx_del.pub_key(), 100),
-    );
-    asset_schema.assets().put(
-        &"asset_2".to_string(),
-        AssetInfo::new(tx_del.pub_key(), 17),
-    );
-    {
-        let assets = asset_schema.assets();
-        for info in assets.iter() {
-            println!("Put info: {:?}", info);
+        let mut asset_schema = AssetSchema { view: &mut db.fork() };
+        asset_schema.assets().put(
+            &"asset_1".to_string(),
+            AssetInfo::new(tx_del.pub_key(), 100),
+        );
+        asset_schema.assets().put(
+            &"asset_2".to_string(),
+            AssetInfo::new(tx_del.pub_key(), 17),
+        );
+        {
+            let assets = asset_schema.assets();
+            for info in assets.iter() {
+                println!("Put info: {:?}", info);
+            }
+        }
+
+        let mut wallet_schema = WalletSchema { view: &mut asset_schema.view };
+
+        let assets = vec![
+            Asset::new("asset_1", 100),
+            Asset::new("asset_2", 17),
+        ];
+
+        let wallet = Wallet::new(tx_del.pub_key(), 100, assets);
+        wallet_schema.wallets().put(tx_del.pub_key(), wallet);
+
+        tx_del.execute(&mut wallet_schema.view);
+
+        if let Some(wallet) = wallet_schema.wallet(tx_del.pub_key()) {
+            assert!(wallet.in_wallet_assets(&vec![
+                                        Asset::new("asset_1", 55)
+            ]));
+            assert!(!wallet.in_wallet_assets(&vec![
+                                         Asset::new("asset_2", 0)
+            ]));
+        } else {
+            panic!("Something wrong!!!");
         }
     }
 
-    let mut wallet_schema = WalletSchema { view: &mut asset_schema.view };
+    #[test]
+    fn negative_delete_assets_test() {
+        let tx_del: TxDelAsset = ::serde_json::from_str(&get_json()).unwrap();
 
-    let assets = vec![
-        Asset::new("asset_1", 100),
-        Asset::new("asset_2", 17),
-    ];
+        let db = Box::new(MemoryDB::new());
+        let mut wallet_schema = WalletSchema { view: &mut db.fork() };
 
-    let wallet = Wallet::new(tx_del.pub_key(), 100, assets);
-    wallet_schema.wallets().put(tx_del.pub_key(), wallet);
+        let assets = vec![
+            Asset::new("asset_1", 400),
+        ];
 
-    tx_del.execute(&mut wallet_schema.view);
+        let wallet = Wallet::new(tx_del.pub_key(), 100, assets);
+        wallet_schema.wallets().put(tx_del.pub_key(), wallet);
 
-    if let Some(wallet) = wallet_schema.wallet(tx_del.pub_key()) {
-        assert!(wallet.in_wallet_assets(&vec![
-            Asset::new("asset_1", 55)
-        ]));
-        assert!(!wallet.in_wallet_assets(&vec![
-            Asset::new("asset_2", 0)
-        ]));
-    } else {
-        panic!("Something wrong!!!");
+        tx_del.execute(&mut wallet_schema.view);
+
+        if let Some(wallet) = wallet_schema.wallet(tx_del.pub_key()) {
+            assert!(wallet.in_wallet_assets(&vec![
+                                        Asset::new("asset_1", 400)
+            ]));
+        } else {
+            panic!("Something wrong!!!");
+        }
     }
-}
 
-#[test]
-fn negative_delete_assets_test() {
-    let tx_del: TxDelAsset = ::serde_json::from_str(&get_json()).unwrap();
-
-    let db = Box::new(MemoryDB::new());
-    let mut wallet_schema = WalletSchema { view: &mut db.fork() };
-
-    let assets = vec![
-        Asset::new("asset_1", 400),
-    ];
-
-    let wallet = Wallet::new(tx_del.pub_key(), 100, assets);
-    wallet_schema.wallets().put(tx_del.pub_key(), wallet);
-
-    tx_del.execute(&mut wallet_schema.view);
-
-    if let Some(wallet) = wallet_schema.wallet(tx_del.pub_key()) {
-        assert!(wallet.in_wallet_assets(&vec![
-            Asset::new("asset_1", 400)
-        ]));
-    } else {
-        panic!("Something wrong!!!");
+    #[test]
+    fn add_asset_info_test() {
+        let tx: TxDelAsset = ::serde_json::from_str(&get_json()).unwrap();
+        assert_eq!(FEE_FOR_MINING, tx.info()["tx_fee"]);
     }
-}
 
-#[test]
-fn add_asset_info_test() {
-    let tx: TxDelAsset = ::serde_json::from_str(&get_json()).unwrap();
-    assert_eq!(FEE_FOR_MINING, tx.info()["tx_fee"]);
 }
