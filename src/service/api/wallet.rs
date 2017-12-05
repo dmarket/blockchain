0fcd37a52f2996da370c3b2a3781a2dd6591fea4
Taras Shchybovyk <tshchybo@gmail.com>
2017-12-05 13:08:53 +0200

    DMB-48 Modify endpoint /wallet/:pub_key
    Renamed `/wallet/:pub_key` to `/wallets/:pub_key`
    Added new endpoint `/wallets/:pub_key/assets` that accepts pagination query parameters `offset` and `limit`
    Moved out pagination functionality into separate function `fn apply_pagination<T: Clone>(req: &mut Request, elements: &Vec<T>) -> Vec<T>`

--- src/service/api/wallet.rs
+++ src/service/api/wallet.rs
@@ -15,7 +15,9 @@ use iron::prelude::*;
 use router::Router;
 
 use service::schema::wallet::WalletSchema;
-use service::wallet::Wallet;
+use service::wallet::{Wallet, Asset};
+
+use self::params::{Params, FromValue};
 
 const PARAMETER_OFFSET_KEY: &str = "offset";
 const PARAMETER_LIMIT_KEY: &str = "limit";
@@ -43,10 +45,38 @@ impl WalletApi {
             Some(wallets)
         }
     }
+
+    fn get_assets(&self, pub_key: &PublicKey) -> Option<Vec<Asset>> {
+        match self.get_wallet(pub_key) {
+            Some(wallet) => Some(wallet.assets()),
+            None => None,
+        }
+    }
 }
 
 impl Api for WalletApi {
     fn wire(&self, router: &mut Router) {
+
+        fn apply_pagination<T: Clone>(req: &mut Request, elements: &Vec<T>) -> Vec<T> {
+            let total_count = elements.len();
+            // read url parameters
+            let parameters = req.get_ref::<Params>().unwrap();
+            let offset_parameter = parameters.get(PARAMETER_OFFSET_KEY);
+            let limit_parameter = parameters.get(PARAMETER_LIMIT_KEY);
+
+            // pagination parameters `offset` and `limit` should be considered together
+            if offset_parameter.is_some() && limit_parameter.is_some() {
+                let offset = FromValue::from_value(offset_parameter.unwrap()).unwrap_or(0);
+                let limit = FromValue::from_value(limit_parameter.unwrap()).unwrap_or(total_count);
+
+                // define wallets that need to be send in responce
+                let from = std::cmp::min(offset, total_count);
+                let to = std::cmp::min(from + limit, total_count);
+                return elements[from..to].to_vec();
+            }
+
+            elements.clone()
+        }
 
         // Gets status of the wallet corresponding to the public key.
         let self_ = self.clone();
@@ -71,34 +101,12 @@ impl Api for WalletApi {
         // Gets status of all wallets.
         let self_ = self.clone();
         let wallets_info = move |req: &mut Request| -> IronResult<Response> {
-            use self::params::{Params, FromValue};
-
             if let Some(wallets) = self_.get_wallets() {
-                // define default values for request parameters
-                let total_wallet_count = wallets.len();
-                let mut wallets_to_send = wallets.clone();
-
-                // read url parameters
-                let parameters = req.get_ref::<Params>().unwrap();
-                let offset_parameter = parameters.get(PARAMETER_OFFSET_KEY);
-                let limit_parameter = parameters.get(PARAMETER_LIMIT_KEY);
-
-                // pagination parameters `offset` and `limit` should be considered together
-                if offset_parameter.is_some() && limit_parameter.is_some() {
-                    let offset = FromValue::from_value(offset_parameter.unwrap()).unwrap_or(0);
-                    let limit = FromValue::from_value(limit_parameter.unwrap()).unwrap_or(
-                        total_wallet_count,
-                    );
-
-                    // define wallets that need to be send in responce
-                    let from = std::cmp::min(offset, total_wallet_count);
-                    let to = std::cmp::min(from + limit, total_wallet_count);
-                    wallets_to_send = wallets_to_send[from..to].to_vec();
-                }
-
+                // apply pagination parameters if they exist
+                let wallets_to_send = apply_pagination(req, &wallets);
                 let wallet_list = serde_json::to_value(&wallets_to_send).unwrap();
                 let response_body = json!({
-                    "total": total_wallet_count,
+                    "total": wallets.len(),
                     "count": wallets_to_send.len(),
                     "wallets": wallet_list,
                 });
@@ -118,7 +126,46 @@ impl Api for WalletApi {
             }
         };
 
+        let self_ = self.clone();
+        let wallet_assets_info = move |req: &mut Request| -> IronResult<Response> {
+            let public_key: PublicKey;
+            {
+                let wallet_key = req.extensions
+                    .get::<Router>()
+                    .unwrap()
+                    .find("pub_key")
+                    .unwrap();
+                public_key = PublicKey::from_hex(wallet_key).map_err(ApiError::FromHex)?;
+            }
+            if let Some(assets) = self_.get_assets(&public_key) {
+                // apply pagination parameters if they exist
+                let assets_to_send = apply_pagination(req, &assets);
+                let assets_list = serde_json::to_value(&assets_to_send).unwrap();
+                let response_body = json!({
+                    "total": assets.len(),
+                    "count": assets_to_send.len(),
+                    "assets": assets_list,
+                });
+
+                let res = self_.ok_response(&serde_json::to_value(response_body).unwrap());
+                let mut res = res.unwrap();
+                res.headers.set(AccessControlAllowOrigin::Any);
+                Ok(res)
+            } else {
+                let res =
+                    self_.not_found_response(&serde_json::to_value("Wallet not found").unwrap());
+                let mut res = res.unwrap();
+                res.headers.set(AccessControlAllowOrigin::Any);
+                Ok(res)
+            }
+        };
+
         router.get("/wallets", wallets_info, "wallets_info");
-        router.get("/wallet/:pub_key", wallet_info, "get_balance");
+        router.get("/wallets/:pub_key", wallet_info, "get_balance");
+        router.get(
+            "/wallets/:pub_key/assets",
+            wallet_assets_info,
+            "assets_info",
+        );
     }
 }
