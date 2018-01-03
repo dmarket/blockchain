cfcdb568a6c0140f82c8d00dcf544964b87f995c
Mikola Samardak <niksaak@gmail.com>
2018-01-03 13:52:07 +0200

    DMB-56: continue fuzzing if fuzz-out dir exists.

--- fuzz.zsh
+++ fuzz.zsh
@@ -1,6 +1,14 @@
 #!/bin/env zsh
 
 mkdir -p fuzz-in
-cargo afl build
-cargo afl run --bin setup-fuzz
-cargo afl fuzz -i fuzz-in -o fuzz-out -m 1024 ./target/debug/fuzz
+cargo afl build &&
+cargo afl run --bin setup-fuzz &&
+if [[ ! -d fuzz-out ]]
+then
+	echo 'Starting new fuzz.'
+	cargo afl fuzz -i fuzz-in -o fuzz-out -m 1024 ./target/debug/fuzz
+else
+	echo 'Continuing fuzz.'
+	cargo afl fuzz -i - -o fuzz-out -m 1024 ./target/debug/fuzz
+fi
+
