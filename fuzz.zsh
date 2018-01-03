6ae9cc3c3cc8932dff5047defc4ddb02e4c057d0
Mikola Samardak <niksaak@gmail.com>
2018-01-03 16:13:18 +0200

    DMB-56: enable parallel fuzzing.

--- fuzz.zsh
+++ fuzz.zsh
@@ -6,9 +6,11 @@ cargo afl run --bin setup-fuzz &&
 if [[ ! -d fuzz-out ]]
 then
 	echo 'Starting new fuzz.'
-	cargo afl fuzz -i fuzz-in -o fuzz-out -m 1024 ./target/debug/fuzz
+	urxvt -e cargo afl fuzz -i fuzz-in -o fuzz-out -m 1024 -M fuzz-master ./target/debug/fuzz &
+	urxvt -e cargo afl fuzz -i fuzz-in -o fuzz-out -m 1024 -S fuzz-slave ./target/debug/fuzz &
 else
 	echo 'Continuing fuzz.'
-	cargo afl fuzz -i - -o fuzz-out -m 1024 ./target/debug/fuzz
+	urxvt -e cargo afl fuzz -i - -o fuzz-out -m 1024 -M fuzz-master ./target/debug/fuzz &
+	urxvt -e cargo afl fuzz -i - -o fuzz-out -m 1024 -S fuzz-slave ./target/debug/fuzz &
 fi
 
