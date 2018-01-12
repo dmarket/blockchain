32f8e565c04ddb0accca3577329103dd916ede2c
Mikola Samardak <niksaak@gmail.com>
2018-01-12 14:43:56 +0200

    DMB-38: add service discovery.

---
+++ service-discovery/src/main.rs
@@ -0,0 +1,22 @@
+extern crate futures;
+extern crate hyper;
+
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+extern crate exonum;
+
+mod sd;
+
+use sd::ServiceDiscovery;
+use hyper::server::Http;
+
+fn main() {
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let server = Http::new()
+        .bind(&addr, || Ok(ServiceDiscovery::new()))
+        .unwrap();
+    server.run().unwrap();
+}
---
+++ service-discovery/src/sd.rs
@@ -0,0 +1,81 @@
+use std::collections::HashSet;
+use std::net::SocketAddr;
+use std::sync::{Arc, RwLock};
+
+use exonum::crypto::PublicKey;
+use futures::future;
+use futures::future::Future;
+use futures::stream::Stream;
+use serde_json;
+
+use hyper;
+use hyper::{Body, Method, StatusCode};
+use hyper::server::{Request, Response, Service};
+
+#[derive(Debug, Hash, Serialize, Deserialize, Eq, PartialEq)]
+pub struct NodeInfo {
+    public: SocketAddr,
+    private: SocketAddr,
+    peer: SocketAddr,
+    consensus: PublicKey,
+    service: PublicKey,
+}
+
+pub struct ServiceDiscovery {
+    nodes: Arc<RwLock<HashSet<NodeInfo>>>,
+}
+
+impl ServiceDiscovery {
+    pub fn new() -> Self {
+        ServiceDiscovery {
+            nodes: Arc::new(RwLock::new(HashSet::new())),
+        }
+    }
+}
+
+impl ServiceDiscovery {
+    fn get_nodes(&self) -> <Self as Service>::Future {
+        match serde_json::to_string_pretty(&*self.nodes.read().unwrap()) {
+            Ok(nodes) => Box::new(future::ok(Response::new().with_body(nodes))),
+            Err(e) => {
+                println!("error in GET: {}", e);
+                Box::new(future::ok(
+                    Response::new().with_status(StatusCode::ImATeapot),
+                ))
+            }
+        }
+    }
+
+    fn post_node(&self, body: Body) -> <Self as Service>::Future {
+        let nodes = Arc::clone(&self.nodes);
+        Box::new(body.concat2().and_then(move |v| {
+            match serde_json::from_slice::<NodeInfo>(&v) {
+                Ok(info) => {
+                    println!("got value: {:?}", &info);
+                    nodes.write().unwrap().insert(info);
+                }
+                Err(e) => println!("error in POST: {}", e),
+            };
+            future::ok(Response::new())
+        }))
+    }
+}
+
+impl Service for ServiceDiscovery {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        println!("Got request: {:?}", req);
+        let response = match (req.method(), req.path()) {
+            (&Method::Get, "/nodes") => self.get_nodes(),
+            (&Method::Post, "/nodes") => self.post_node(req.body()),
+            _ => Box::new(future::ok(
+                Response::new().with_status(StatusCode::NotFound),
+            )),
+        };
+        response
+    }
+}
