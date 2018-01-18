1d1b12684f846566f2341b2896ab62bf697f9657
Mikola Samardak <niksaak@gmail.com>
2018-01-18 17:11:10 +0200

    DMB-38: ascertain compatibility with network reconfiguration.

--- dmbc-node/src/main.rs
+++ dmbc-node/src/main.rs
@@ -1,7 +1,10 @@
 extern crate curl;
-extern crate dmbc;
 extern crate exonum;
 extern crate exonum_configuration;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 
 extern crate dmbc;
 
@@ -9,41 +12,57 @@ mod net_config;
 
 use exonum::blockchain::{ConsensusConfig, GenesisConfig, Service, TimeoutAdjusterConfig,
                          ValidatorKeys};
+use exonum::crypto::{PublicKey, SecretKey};
+use exonum::encoding::serialize::FromHex;
 use exonum::node::{Node, NodeApiConfig, NodeConfig};
 use exonum::storage::{RocksDB, RocksDBOptions};
 use exonum_configuration::ConfigurationService;
 use dmbc::config;
-use dmbc::keys::{Disc, KeyPair};
 use dmbc::service::CurrencyService;
+
+const GENESIS_VALIDATOR_PUBLIC: &'static str =
+    "4e298e435018ab0a1430b6ebd0a0656be15493966d5ce86ed36416e24c411b9f";
+const GENESIS_VALIDATOR_SECRET: &'static str =
+    "a5bc1d2f4de5f48d95e6b607f9975a2021a176e8b9bf74735c92653318ad98d34e298e435018ab0a1430b6ebd0a0656be15493966d5ce86ed36416e24c411b9f";
+const GENESIS_SERVICE_PUBLIC: &'static str =
+    "68e774a4339cccfae644dcf3e44360839c84a6475c7d2943ed59b81d7eb6e9f0";
+const GENESIS_SERVICE_SECRET: &'static str =
+    "b72ae0c3cdea13e3ec510120436aca00e102f47d4232acc8f72bfb516382a53568e774a4339cccfae644dcf3e44360839c84a6475c7d2943ed59b81d7eb6e9f0";
 
 fn main() {
     exonum::helpers::init_logger().unwrap();
 
     /** Create Keys */
-    println!("Current node: {}", config::config().api().current_node());
-    let consensus_name: String = config::config().api().current_node() + "_consensus.json";
-    let service_name: String = config::config().api().current_node() + "_service.json";
+    println!("Initializing node: {}", config::config().api().current_node());
 
-    let consensus_keys = KeyPair::read(&consensus_name);
-    let service_keys = KeyPair::read(&service_name);
+    let consensus_public_key = PublicKey::from_hex(GENESIS_VALIDATOR_PUBLIC).unwrap();
+    let consensus_secret_key = SecretKey::from_hex(GENESIS_VALIDATOR_SECRET).unwrap();
+    let service_public_key = PublicKey::from_hex(GENESIS_SERVICE_PUBLIC).unwrap();
+    let service_secret_key = SecretKey::from_hex(GENESIS_SERVICE_SECRET).unwrap();
 
-    let consensus_public_key = consensus_keys.public;
-    let consensus_secret_key = consensus_keys.secret;
-    let service_public_key = service_keys.public;
-    let service_secret_key = service_keys.secret;
+    let public_api = config::config().api().address().parse().unwrap();
+    let private_api = config::config().api().private_address().parse().unwrap();
+    let peer_address = config::config().api().peer_address().parse().unwrap();
 
-    let nodenames = vec!["node0" /*, "node1", "node2", "node3"*/];
-    let mut validators: Vec<ValidatorKeys> = vec![];
-    for name_prefix in nodenames {
-        let consensus_name = name_prefix.to_string() + "_consensus.json";
-        let service_name = name_prefix.to_string() + "_service.json";
-        let consensus_keys = KeyPair::read(&consensus_name);
-        let service_keys = KeyPair::read(&service_name);
-        validators.push(ValidatorKeys {
-            consensus_key: consensus_keys.public,
-            service_key: service_keys.public,
-        });
-    }
+    let info = net_config::ValidatorInfo {
+        public: public_api,
+        peer: peer_address,
+        consensus: consensus_public_key,
+        service: service_public_key,
+    };
+    eprintln!("Node info: {:?}", &info);
+
+    let peers = match net_config::connect_validator(&info) {
+        Ok(peers) => {
+            eprintln!("Connected as validator, peers: {:?}", &peers);
+            peers
+        },
+        Err(e) => {
+            eprintln!("Unable to connect as validator: {}", &e);
+            eprintln!("Running in loner-mode.");
+            Default::default()
+        },
+    };
 
     let consensus_config = ConsensusConfig {
         round_timeout: 3000,
@@ -58,17 +77,24 @@ fn main() {
     };
 
     // Configure Node
+    let validators = Some(ValidatorKeys {
+        consensus_key: consensus_public_key,
+        service_key: service_public_key,
+    });
+
     let genesis = GenesisConfig::new_with_consensus(consensus_config, validators.into_iter());
     let api_cfg = NodeApiConfig {
-        public_api_address: Some(config::config().api().address().parse().unwrap()),
-        private_api_address: Some(config::config().api().private_address().parse().unwrap()),
+        public_api_address: Some(public_api),
+        private_api_address: Some(private_api),
         ..Default::default()
     };
+
+    let peer_addrs = peers.iter().map(|p| p.peer).collect();
 
     // Complete node configuration
     let node_cfg = NodeConfig {
         listen_address: config::config().api().peer_address().parse().unwrap(),
-        peers: config::config().api().peers(),
+        peers: peer_addrs,
         service_public_key,
         service_secret_key,
         consensus_public_key,
@@ -93,6 +119,8 @@ fn main() {
         Box::new(ConfigurationService::new()),
         Box::new(CurrencyService),
     ];
+
+    eprintln!("Launching node. What can possibly go wrong?");
 
     let node = Node::new(db, services, node_cfg);
     node.run().unwrap();
