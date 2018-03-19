8b81dd91a928c6d28a74d1b854ede938eb4437b4
Ilya Sinyakin <sinyakin.ilya@gmail.com>
2018-03-19 17:39:37 +0200

    DMB-68: add add_assets_insufficient_funds tests.

--- dmbc/tests/transactions/add_assets.rs
+++ dmbc/tests/transactions/add_assets.rs
@@ -9,6 +9,8 @@ use dmbc::currency::assets::{AssetBundle, MetaAsset, AssetId};
 use dmbc::currency::transactions::builders::fee;
 use dmbc::currency::transactions::builders::transaction;
 use dmbc::currency::configuration::TransactionFees;
+use dmbc::currency::error::Error;
+
 use transactions::*;
 
 #[test]
@@ -323,7 +325,6 @@ fn add_assets() {
 
 #[test]
 fn add_assets_with_different_fees() {
-    use dmbc::currency::error::Error;
     let mut testkit = init_testkit();
     let api = testkit.api();
 
@@ -417,6 +418,62 @@ fn add_assets_with_different_fees() {
 
     let bc_asset_info = get_asset_info(&api, &asset_id).unwrap();
     assert_eq!(meta_asset.to_info(&public_key), bc_asset_info);
+
+
+}
+
+#[test]
+fn add_assets_insufficient_funds() {
+    let mut testkit = init_testkit();
+    let api = testkit.api();
+
+    set_configuration(&mut testkit, TransactionFees::new(10, 1_00_000_000, 0, 0, 0, 0));
+
+    let (public_key, secret_key) = crypto::gen_keypair();
+
+    let fees = fee::Builder::new()
+        .trade(10, 10)
+        .exchange(10, 10)
+        .transfer(10, 10)
+        .build();
+
+    let meta_data = r#"{"name":"test_item","type":"skin","category":"gun","image":"http://test.com/test_item.jpg"}"#;
+    let meta_asset = MetaAsset::new(
+        &public_key,
+        meta_data,
+        1,
+        fees.clone()
+    );
+
+    let tx_add_assets = transaction::Builder::new()
+        .keypair(public_key, secret_key.clone())
+        .tx_add_assets()
+        .add_asset_value(meta_asset.clone())
+        .seed(85)
+        .build();
+
+    post_tx(&api, &tx_add_assets);
+
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_add_assets.hash());
+    assert_eq!(Ok(Err(Error::InsufficientFunds)), s);
+
+
+    let mine_1_dmc = transaction::Builder::new()
+        .keypair(public_key, secret_key.clone())
+        .tx_mine()
+        .build();
+
+    post_tx(&api, &mine_1_dmc);
+    testkit.create_block();
+
+    post_tx(&api, &tx_add_assets);
+
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_add_assets.hash());
+    assert_eq!(Ok(Err(Error::InsufficientFunds)), s);
 
 
 }
