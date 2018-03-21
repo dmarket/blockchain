5e8c7bec11bb895c56b0bcf9e50f57b5f61d5e2b
Ilya Sinyakin <sinyakin.ilya@gmail.com>
2018-03-21 17:37:36 +0200

    DMB-68: add exchange_asset_insufficient_funds test

--- dmbc/tests/transactions/exchange.rs
+++ dmbc/tests/transactions/exchange.rs
@@ -11,6 +11,7 @@ use dmbc::currency::transactions::builders::fee;
 use dmbc::currency::transactions::builders::transaction;
 use dmbc::currency::configuration::TransactionFees;
 use dmbc::currency::transactions::components::FeeStrategy;
+use dmbc::currency::error::Error;
 
 use transactions::*;
 
@@ -267,6 +268,79 @@ fn exchange_asset_fee_strategy() {
 
     let recipient_wallet = get_wallet(&api, &recipient_pk);
     assert_eq!(a, recipient_wallet.assets());
+    assert_eq!(recipient_balance, recipient_wallet.balance());
+
+    let genesis_wallet = get_wallet(&api, &Service::genesis_wallet());
+    assert_eq!(genesis_balance, genesis_wallet.balance());
+
+    let creator_wallet = get_wallet(&api, &creator_pk);
+    assert_eq!(creator_balance, creator_wallet.balance());
+}
+
+#[test]
+fn exchange_asset_insufficient_funds_fee_asset_very_big() {
+    let mut testkit = init_testkit();
+    let api = testkit.api();
+    set_configuration(&mut testkit, TransactionFees::new(0, 0, 0, 1000, 0, 0));
+
+    let (sender_pk, sender_sk) = mine_wallet(&mut testkit);
+    let (recipient_pk, recipient_sk) = mine_wallet(&mut testkit);
+    let (creator_pk, creator_sk) = crypto::gen_keypair();
+    let mut sender_balance = DMC_1;
+    let mut recipient_balance = DMC_1;
+    let mut creator_balance = 0u64;
+    let mut genesis_balance = 137_000_000_00000000u64;
+
+    let tx_add_assets = transaction::Builder::new()
+        .keypair(creator_pk, creator_sk.clone())
+        .tx_add_assets()
+        .add_asset_receiver(sender_pk, "asset1", 1, exchange_fee(DMC_1,1000))
+        .build();
+    let asset = AssetBundle::from_data("asset1", 1, &creator_pk);
+
+    post_tx(&api, &tx_add_assets);
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_add_assets.hash());
+    assert_eq!(Ok(Ok(())), s);
+
+    let e:Vec<AssetBundle> = Vec::new();
+    let a = vec![asset.clone()];
+
+    let sender_wallet = get_wallet(&api, &sender_pk);
+    assert_eq!(a, sender_wallet.assets());
+    assert_eq!(DMC_1, sender_wallet.balance());
+
+    let recipient_wallet = get_wallet(&api, &recipient_pk);
+    assert_eq!(e, recipient_wallet.assets());
+    assert_eq!(DMC_1, recipient_wallet.balance());
+
+    let tx_exchange_assets = transaction::Builder::new()
+        .keypair(recipient_pk, recipient_sk.clone())
+        .tx_exchange()
+        .sender(sender_pk)
+        .sender_secret(sender_sk.clone())
+        .fee_strategy(FeeStrategy::Recipient)
+        .sender_add_asset_value(asset.clone())
+        .build();
+
+    post_tx(&api, &tx_exchange_assets);
+    testkit.create_block();
+
+    recipient_balance -= 1000;
+    genesis_balance += 1000;
+//c    creator_balance += ;
+
+
+    let s = get_status(&api, &tx_exchange_assets.hash());
+    assert_eq!(Ok(Err(Error::InsufficientFunds)), s);
+
+    let sender_wallet = get_wallet(&api, &sender_pk);
+    assert_eq!(a, sender_wallet.assets());
+    assert_eq!(sender_balance, sender_wallet.balance());
+
+    let recipient_wallet = get_wallet(&api, &recipient_pk);
+    assert_eq!(e, recipient_wallet.assets());
     assert_eq!(recipient_balance, recipient_wallet.balance());
 
     let genesis_wallet = get_wallet(&api, &Service::genesis_wallet());
