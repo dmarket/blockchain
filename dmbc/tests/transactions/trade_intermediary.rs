222dc6c05ef6163bda650f4eac9cea011e4d4572
Taras Shchybovyk <tshchybo@gmail.com>
2018-03-26 11:58:07 +0300

    DMB-68: Add more test to trade intermediary transaction.

--- dmbc/tests/transactions/trade_intermediary.rs
+++ dmbc/tests/transactions/trade_intermediary.rs
@@ -7,7 +7,7 @@ use exonum::messages::Message;
 use dmbc::currency::transactions::builders::fee;
 use dmbc::currency::configuration::TransactionFees;
 use dmbc::currency::transactions::components::FeeStrategy;
-// use dmbc::currency::error::Error;
+use dmbc::currency::error::Error;
 
 use transactions::*;
 
@@ -250,5 +250,189 @@ fn trade_intermediary_fee_strategy() {
 
 #[test]
 fn trade_interemedary_insufficient_funds() {
+    let mut testkit = init_testkit();
+    let api = testkit.api();
+    let transaction_fee = 100;
+    let units = 2;
+    let price_per_unit = 1000;
+    let commission = 50;
+    let trade_tax = 0;
+    let trade_ratio = 1;
 
+    set_configuration(&mut testkit, TransactionFees::new(0, 0, 0, 0, transaction_fee, 0));
+
+    // Sender pays
+    let meta_data = "asset";
+    let (seller_public_key, seller_secret_key) = WalletMiner::new().mine_empty();
+
+    let (buyer_public_key, buyer_secret_key) = WalletMiner::new().mine(&mut testkit);
+    let (intermediary_public_key, intermediary_secret_key) = WalletMiner::new().mine_empty();
+
+    let tx_trade = transaction::Builder::new()
+        .keypair(buyer_public_key, buyer_secret_key)
+        .tx_trade_assets_with_intermediary()
+        .intermediary_key_pair(intermediary_public_key, intermediary_secret_key)
+        .commission(commission)
+        .add_asset(meta_data, units, price_per_unit)
+        .seller(seller_public_key, seller_secret_key)
+        .fee_strategy(FeeStrategy::Sender)
+        .seed(1)
+        .data_info("trade_test")
+        .build();
+
+    post_tx(&api, &tx_trade);
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_trade.hash());
+    assert_eq!(Ok(Err(Error::InsufficientFunds)), s);
+
+    // Recipient pays
+    let (seller_public_key, seller_secret_key) = WalletMiner::new()
+        .add_asset(meta_data, units, get_trade_fee(trade_tax, trade_ratio))
+        .mine(&mut testkit);
+
+    let (buyer_public_key, buyer_secret_key) = WalletMiner::new().mine_empty();
+    let (intermediary_public_key, intermediary_secret_key) = WalletMiner::new().mine_empty();
+
+    let tx_trade = transaction::Builder::new()
+        .keypair(buyer_public_key, buyer_secret_key)
+        .tx_trade_assets_with_intermediary()
+        .intermediary_key_pair(intermediary_public_key, intermediary_secret_key)
+        .commission(commission)
+        .add_asset(meta_data, units, price_per_unit)
+        .seller(seller_public_key, seller_secret_key)
+        .fee_strategy(FeeStrategy::Recipient)
+        .seed(1)
+        .data_info("trade_test")
+        .build();
+
+    post_tx(&api, &tx_trade);
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_trade.hash());
+    assert_eq!(Ok(Err(Error::InsufficientFunds)), s);
+
+    // Recipient and Sender pay
+    let (seller_public_key, seller_secret_key) = WalletMiner::new().mine_empty();
+    let (buyer_public_key, buyer_secret_key) = WalletMiner::new().mine_empty();
+    let (intermediary_public_key, intermediary_secret_key) = WalletMiner::new().mine_empty();
+
+    let tx_trade = transaction::Builder::new()
+        .keypair(buyer_public_key, buyer_secret_key)
+        .tx_trade_assets_with_intermediary()
+        .intermediary_key_pair(intermediary_public_key, intermediary_secret_key)
+        .commission(commission)
+        .add_asset(meta_data, units, price_per_unit)
+        .seller(seller_public_key, seller_secret_key)
+        .fee_strategy(FeeStrategy::RecipientAndSender)
+        .seed(1)
+        .data_info("trade_test")
+        .build();
+
+    post_tx(&api, &tx_trade);
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_trade.hash());
+    assert_eq!(Ok(Err(Error::InsufficientFunds)), s);
+
+    // Intermediary pays
+    let (seller_public_key, seller_secret_key) = WalletMiner::new().mine(&mut testkit);
+    let (buyer_public_key, buyer_secret_key) = WalletMiner::new().mine(&mut testkit);
+    let (intermediary_public_key, intermediary_secret_key) = WalletMiner::new().mine_empty();
+
+    let tx_trade = transaction::Builder::new()
+        .keypair(buyer_public_key, buyer_secret_key)
+        .tx_trade_assets_with_intermediary()
+        .intermediary_key_pair(intermediary_public_key, intermediary_secret_key)
+        .commission(commission)
+        .add_asset(meta_data, units, price_per_unit)
+        .seller(seller_public_key, seller_secret_key)
+        .fee_strategy(FeeStrategy::Intermediary)
+        .seed(1)
+        .data_info("trade_test")
+        .build();
+
+    post_tx(&api, &tx_trade);
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_trade.hash());
+    assert_eq!(Ok(Err(Error::InsufficientFunds)), s);
+}
+
+#[test]
+fn trade_interemedary_assets_not_found() {
+    let mut testkit = init_testkit();
+    let api = testkit.api();
+    let transaction_fee = 100;
+    let units = 2;
+    let price_per_unit = 1000;
+    let commission = 50;
+
+    set_configuration(&mut testkit, TransactionFees::new(0, 0, 0, 0, transaction_fee, 0));
+
+    // Sender pays
+    let meta_data = "asset";
+    let (seller_public_key, seller_secret_key) = WalletMiner::new().mine(&mut testkit);
+
+    let (buyer_public_key, buyer_secret_key) = WalletMiner::new().mine(&mut testkit);
+    let (intermediary_public_key, intermediary_secret_key) = WalletMiner::new().mine_empty();
+
+    let tx_trade = transaction::Builder::new()
+        .keypair(buyer_public_key, buyer_secret_key)
+        .tx_trade_assets_with_intermediary()
+        .intermediary_key_pair(intermediary_public_key, intermediary_secret_key)
+        .commission(commission)
+        .add_asset(meta_data, units, price_per_unit)
+        .seller(seller_public_key, seller_secret_key)
+        .fee_strategy(FeeStrategy::Sender)
+        .seed(1)
+        .data_info("trade_test")
+        .build();
+
+    post_tx(&api, &tx_trade);
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_trade.hash());
+    assert_eq!(Ok(Err(Error::AssetNotFound)), s);
+}
+
+#[test]
+fn trade_interemedary_insufficient_assets() {
+    let mut testkit = init_testkit();
+    let api = testkit.api();
+    let transaction_fee = 100;
+    let units = 2;
+    let price_per_unit = 1000;
+    let commission = 50;
+    let trade_tax = 0;
+    let trade_ratio = 1;
+
+    set_configuration(&mut testkit, TransactionFees::new(0, 0, 0, 0, transaction_fee, 0));
+
+    // Sender pays
+    let meta_data = "asset";
+    let (seller_public_key, seller_secret_key) = WalletMiner::new()
+        .add_asset(meta_data, units - 1, get_trade_fee(trade_tax, trade_ratio))
+        .mine(&mut testkit);
+
+    let (buyer_public_key, buyer_secret_key) = WalletMiner::new().mine(&mut testkit);
+    let (intermediary_public_key, intermediary_secret_key) = WalletMiner::new().mine_empty();
+
+    let tx_trade = transaction::Builder::new()
+        .keypair(buyer_public_key, buyer_secret_key)
+        .tx_trade_assets_with_intermediary()
+        .intermediary_key_pair(intermediary_public_key, intermediary_secret_key)
+        .commission(commission)
+        .add_asset(meta_data, units, price_per_unit)
+        .seller(seller_public_key, seller_secret_key)
+        .fee_strategy(FeeStrategy::Sender)
+        .seed(1)
+        .data_info("trade_test")
+        .build();
+
+    post_tx(&api, &tx_trade);
+    testkit.create_block();
+
+    let s = get_status(&api, &tx_trade.hash());
+    assert_eq!(Ok(Err(Error::InsufficientAssets)), s);
 }
