8495592dec617287fff6ad3562f8ba7ad151e0d7
Taras Shchybovyk <tshchybo@gmail.com>
2018-04-05 11:54:37 +0300

    DMB-87: add inter public key error check.

--- dmbc/src/currency/api/assets_intern.rs
+++ dmbc/src/currency/api/assets_intern.rs
@@ -7,7 +7,7 @@ extern crate serde_json;
 
 use std::collections::HashMap;
 
-use exonum::api::{Api, ApiError as ExonumApiError};
+use exonum::api::Api;
 use exonum::crypto::PublicKey;
 use exonum::encoding::serialize::FromHex;
 use iron::headers::AccessControlAllowOrigin;
@@ -52,13 +52,13 @@ impl Api for AssetInternApi {
     fn wire(&self, router: &mut Router) {
 
         let get_asset_id = move |req: &mut Request| -> IronResult<Response> {
-            let public_key = {
+            let public_key_result = {
                 let wallet_key = req.extensions
                     .get::<Router>()
                     .unwrap()
                     .find("pub_key")
                     .unwrap();
-                PublicKey::from_hex(wallet_key).map_err(ExonumApiError::FromHex)?
+                PublicKey::from_hex(wallet_key)
             };
             let meta_data = {
                 req.extensions
@@ -67,10 +67,15 @@ impl Api for AssetInternApi {
                     .find("meta_data")
                     .unwrap()
             };
-            let id = AssetId::from_data(meta_data, &public_key);
-            let mut assets = HashMap::<String, String>::new();
-            assets.insert(meta_data.to_string(), id.to_string());
-            let result: AssetIdResponse = Ok(Ok(AssetIdResponseBody { assets }));
+            let result: AssetIdResponse = match public_key_result {
+                Ok(public_key) => {
+                    let id = AssetId::from_data(meta_data, &public_key);
+                    let mut assets = HashMap::<String, String>::new();
+                    assets.insert(meta_data.to_string(), id.to_string());
+                    Ok(Ok(AssetIdResponseBody { assets }))
+                },
+                Err(_) => Err(ApiError::WalletHexInvalid)
+            };
 
             let status_code =
                 result.clone()
@@ -90,25 +95,30 @@ impl Api for AssetInternApi {
         };
 
         let get_asset_ids_for_key = move |req: &mut Request| -> IronResult<Response> {
-            let public_key = {
+            let public_key_result = {
                 let wallet_key = req.extensions
                     .get::<Router>()
                     .unwrap()
                     .find("pub_key")
                     .unwrap();
-                PublicKey::from_hex(wallet_key).map_err(ExonumApiError::FromHex)?
+                PublicKey::from_hex(wallet_key)
             };
-            let result: AssetIdResponse = match req.get::<bodyparser::Struct<AssetIdRequest>>() {
-                Ok(Some(request)) => {
-                    let mut assets = HashMap::<String, String>::new();
-                    for asset in request.assets {
-                        let id = AssetId::from_data(&asset, &public_key);
-                        assets.insert(asset, id.to_string());
+            let result: AssetIdResponse = match public_key_result {
+                Ok(public_key) => {
+                    match req.get::<bodyparser::Struct<AssetIdRequest>>() {
+                        Ok(Some(request)) => {
+                            let mut assets = HashMap::<String, String>::new();
+                            for asset in request.assets {
+                                let id = AssetId::from_data(&asset, &public_key);
+                                assets.insert(asset, id.to_string());
+                            }
+                            Ok(Ok(AssetIdResponseBody { assets }))
+                        },
+                        Ok(None) => Err(ApiError::EmptyRequestBody),
+                        Err(_) => Err(ApiError::IncorrectRequest),
                     }
-                    Ok(Ok(AssetIdResponseBody { assets }))
                 },
-                Ok(None) => Err(ApiError::EmptyRequestBody),
-                Err(_) => Err(ApiError::IncorrectRequest),
+                Err(_) => Err(ApiError::WalletHexInvalid)
             };
 
             let status_code =
@@ -132,16 +142,25 @@ impl Api for AssetInternApi {
             let result: AssetIdBatchResponse = match req.get::<bodyparser::Struct<AssetIdBatchRequest>>() {
                 Ok(Some(request)) => {
                     let mut assets_batch = HashMap::<String, HashMap<String, String>>::new();
+                    let mut invalid_hex = false;
                     for (key, assets_data) in request.assets {
-                        let public_key = PublicKey::from_hex(key.clone()).map_err(ExonumApiError::FromHex)?;
+                        let public_key = PublicKey::from_hex(key.clone());
+                        if public_key.is_err() {
+                            invalid_hex = true;
+                            break;
+                        }
+
                         let mut assets = HashMap::<String, String>::new();
                         for asset in assets_data {
-                            let id = AssetId::from_data(&asset, &public_key);
+                            let id = AssetId::from_data(&asset, &public_key.unwrap());
                             assets.insert(asset, id.to_string());
                         }
                         assets_batch.insert(key, assets);
                     }
-                    Ok(Ok(AssetIdBatchResponseBody { assets: assets_batch }))
+                    match invalid_hex {
+                        false => Ok(Ok(AssetIdBatchResponseBody { assets: assets_batch })),
+                        true => Err(ApiError::WalletHexInvalid)
+                    }
                 },
                 Ok(None) => Err(ApiError::EmptyRequestBody),
                 Err(_) => Err(ApiError::IncorrectRequest),
