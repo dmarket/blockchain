8eeec503a110ae4b2db6fd87b5d0a67ecb19d2cd
Taras Shchybovyk <tshchybo@gmail.com>
2018-04-20 12:52:19 +0300

    DMB-86: Rewrite trade assets tests.

--- dmbc/tests/common/mod.rs
+++
@@ -1,7 +0,0 @@
-use dmbc::currency::configuration::GENESIS_WALLET_PUB_KEY;
-use exonum::crypto::PublicKey;
-use exonum::encoding::serialize::FromHex;
-
-pub fn default_genesis_wallet() -> PublicKey {
-    PublicKey::from_hex(GENESIS_WALLET_PUB_KEY).unwrap()
-}
