ed80643dfba13b82dfcde2fda7fe49c714d34483
Ilya Sinyakin <sinyakin.ilya@gmail.com>
2018-05-11 16:43:18 +0300

    DMB-115: add flag version.

--- dmbc-node/src/flag.rs
+++ dmbc-node/src/flag.rs
@@ -1,1 +1,25 @@
 extern crate clap;
+
+use clap::{App, Arg};
+use super::VERSION;
+
+pub struct Flags {}
+
+pub fn parse() -> Option<Flags> {
+    let matches = App::new("DMarket blockchain")
+        .arg(Arg::with_name("version")
+            .help("show version")
+            .short("v")
+            .long("version")
+            .multiple(false)
+        )
+        .get_matches();
+
+    match matches.occurrences_of("version") {
+        1 => {
+            println!("DMarket Blockchain v{}", VERSION);
+            None
+        }
+        _ => return Some(Flags {}),
+    }
+}
